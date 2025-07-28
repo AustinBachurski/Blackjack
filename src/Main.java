58655c1ff15a8026e80a5598ea3196a8950dbbe8
abachurski <austinbachurski@gmail.com>
2025-07-28 16:36:40 -0600

    Create game object and play.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
-import Cards.*;
-
 public class Main {
     public static void main(String[] args) {
-        Card card = new Card(CardFace.KING, CardSuite.HEARTS);
-
-        for (String line : card._art)
-        System.out.println(line);
+        Game game = new Game();
+        game.play();
     }
 }
-
